a1a3d998e8bc0d10b5c3e43139625635293ade36
Rotem Eidlitz <rotemchaya@gmail.com>
2024-12-26 14:08:58 +0200

    going real time

--- mock-srv/app.js
+++ mock-srv/app.js
@@ -2,6 +2,7 @@ import path from 'node:path'
 import { fileURLToPath } from 'node:url'
 import AutoLoad from '@fastify/autoload'
 import cors from '@fastify/cors'
+import websocket from "@fastify/websocket";
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
@@ -12,6 +13,9 @@ export const options = {}
 export default async function (fastify, opts) {
   // Place here your custom code!
   fastify.register(cors)
+    // Register Websocket
+    fastify.register(websocket, {});
+    
   // Do not touch the following lines
 
   // This loads all plugins defined in plugins
--- mock-srv/plugins/data-utils.js
+++ mock-srv/plugins/data-utils.js
@@ -1,23 +1,77 @@
 "use strict";
-
+import fastify from "fastify";
 import fp from "fastify-plugin";
+import { PassThrough } from "node:stream";
 
+ 
+
+// Mock data
+const orders = {
+  A1: { total: 3 },
+  A2: { total: 7 },
+  B1: { total: 101 },
+};
+
+// Map category to ID prefix
 const catToPrefix = {
   electronics: "A",
   confectionery: "B",
 };
 
+// Create a stream of orders
+const orderStream = new PassThrough({ objectMode: true });
+
+// Simulate real-time orders
+async function* realtimeOrdersSimulator() {
+  for await (const { id, total } of orderStream) {
+    yield JSON.stringify({ id, total });
+  }
+}
+
+// Add order to stream and update total
+function addOrder(id, amount) {
+  if (orders.hasOwnProperty(id) === false) {
+    const err = new Error(`Order ${id} not found`);
+    err.status = 404;
+    throw err;
+  }
+  if (Number.isInteger(amount) === false) {
+    const err = new Error('Supplied amount must be an integer');
+    err.status = 400;
+    throw err;
+  }
+  orders[id].total += amount;
+  const { total } = orders[id]
+  orderStream.write({ id, total });
+}
+
+// Return current orders
+function* currentOrders(category) {
+  const idPrefix = catToPrefix[category];
+  if (!idPrefix) return;
+  const ids = Object.keys(orders).filter((id) => id[0] === idPrefix);
+  for (const id of ids) {
+    yield JSON.stringify({ id, ...orders[id] });
+  }
+}
+
+// Calculate next ID
 const calculateID = (idPrefix, data) => {
   const sorted = [...new Set(data.map(({ id }) => id))];
   const next = Number(sorted.pop().slice(1)) + 1;
   return `${idPrefix}${next}`;
 };
 
+// Plugin
 export default fp(async function (fastify, opts) {
+  debugger
+  fastify.decorate("currentOrders", currentOrders);
+  fastify.decorate("realtimeOrders", realtimeOrdersSimulator);
+  fastify.decorate("addOrder", addOrder);
   fastify.decorate("mockDataInsert", function (request, category, data) {
     const idPrefix = catToPrefix[category];
     const id = calculateID(idPrefix, data);
     data.push({ id, ...request.body });
-    return data
+    return data;
   });
 });
